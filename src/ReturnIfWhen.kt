a06ff42dd51e122a8b744890624440a25a111e81
Diki Faturrohman <118197085+DikiFaturrohman@users.noreply.github.com>
2025-03-20 09:15:08 +0700

    Return If When

---
+++ src/ReturnIfWhen.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    fun sayHello(name: String = ""): String{
+        return if(name == "") {
+            "Hello Bro!"
+        } else {
+            "Hello $name"
+        }
+    }
+
+    println(sayHello())
+    println(sayHello("Diki"))
+}
