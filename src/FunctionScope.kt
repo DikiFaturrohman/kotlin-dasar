9b56ecb41d1b8315be31daf144f5e0f90aaae9e7
Diki Faturrohman <118197085+DikiFaturrohman@users.noreply.github.com>
2025-03-20 09:05:34 +0700

    Function Scope

---
+++ src/FunctionScope.kt
@@ -0,0 +1,15 @@
+fun satu(){
+   // contohHelloWorld()
+}
+
+fun dua(){
+   // contohHelloWorld()
+}
+
+fun main() {
+    fun contohHelloWorld(){
+        println("Hello World")
+    }
+
+    contohHelloWorld()
+}
